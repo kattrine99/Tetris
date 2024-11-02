db93bb6514f04d427773e94847b4ad6c3fe07319
Ekaterina <kattren990703@gmail.com>
2024-11-02 16:26:15 +0500

    added rotation for figures

--- script.js
+++ script.js
@@ -1,103 +1,103 @@
-function drawTetrisPlayground(x,y, target){
-    if(x <= 0 || y <= 0)throw new Error('x and y cannot be negative')
-     
-    if(target.children.length) throw new Error('Aborted: Target element should be empty!')    
-    for (rowCount = 0 ; rowCount < y ; rowCount++){
+import {shapes} from "./shapes.js"
+ 
+function drawTetrisPlayground(x, y, target) {
+    if (x <= 0 || y <= 0) throw new Error('x and y cannot be negative')
+
+    if (target.children.length) throw new Error('Aborted: target element should be empty')
+
+    for (let rowsCount = 0; rowsCount < y; rowsCount++) {
         const row = document.createElement('div')
         row.className = 'row'
-        row.dataset['row'] = rowCount
-        row.style.transform = `translateY(${-rowCount}px)`
-        for (let cellsCount = 0 ; cellsCount < x; cellsCount++){
+        row.dataset['row'] = rowsCount
+        row.style.transform = `translateY(${-rowsCount}px)`
+
+
+        for (let cellsCount = 0; cellsCount < x; cellsCount++) {
             const cell = document.createElement('div')
             cell.className = 'cell'
             cell.dataset['cell'] = cellsCount
             cell.style.transform = `translateX(${-cellsCount}px)`
             row.append(cell)
         }
+
         target.append(row)
     }
 }
 
-const tetrisPlayGroundTarget = document.querySelector('.tetris-play-ground')
-try{
-    drawTetrisPlayground(10,20,tetrisPlayGroundTarget)
-}
-catch(e){
+const tetrisPlaygroundTarget = document.querySelector('.tetris-play-ground')
+
+try {
+    drawTetrisPlayground(10, 20, tetrisPlaygroundTarget)
+} catch (e) {
     console.log(e.message)
 }
 
-const shapes = {
-    O:{ 
-        shape:[[1, 1],
-               [1, 1]],
-        color: 'yellow'
-    },
-
-    J:{ 
-        shape:[[0, 1],
-               [0, 1],
-               [1, 1]],
-        color: 'navy'
-    },
-
-    I: { 
-        shape:[[1],
-               [1],
-               [1],
-               [1]],
-        color: 'lightblue'
-    },
-
-    L: { 
-        shape:[[1, 0],
-               [1, 0],
-               [1, 1]],
-        color: 'orange'
-    },
-    S: { 
-        shape:[[0, 1, 1],
-               [1, 1, 0]],
-        color: 'yellowgreen'
-    },
-
-    T: { 
-        shape:[[1, 1, 1],
-               [0, 1, 0]],
-        color: 'purple'
-    },
-
-    Z:{ 
-        shape:[[1, 1, 0],
-               [0, 1, 1]],
-        color: 'red'
-    }
-}
 const shapeKeys = Object.keys(shapes)
-const shapeKeyIndex = Math.floor(Math.random()*shapeKeys.length)
+
+const shapeKeyIndex = Math.floor(Math.random() * shapeKeys.length)
+
 const shapeKey = shapeKeys[shapeKeyIndex]
 
 const currentShape = shapes[shapeKey]
-const rowsToColor = currentShape.shape.length
-const cellsToColor = currentShape.shape[0].length
 
-for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
-    const row = tetrisPlayGroundTarget.children[rowIndex];
+function renderShape() {
+    const rowsToColor = currentShape.shape.length
+    const cellsToColor = currentShape.shape[0].length
 
-    for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
-        const cell = row.children[cellIndex];
-        if (currentShape.shape[rowIndex][cellIndex]) {
-            cell.style.backgroundColor = currentShape.color;
+    for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
+        const row = tetrisPlaygroundTarget.children[rowIndex]
+
+        for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
+            const cell = row.children[cellIndex]
+            if (currentShape.shape[rowIndex][cellIndex]) {
+                cell.style.backgroundColor = currentShape.color
+            }
         }
     }
 }
 
 
+function rotateShape(shape) {
+    if (shape.length === 2 && shape[0].length === 2) return shape
 
+    const rotatedShape = []
 
+    for (let rowsCount = 0; rowsCount < shape[0].length; rowsCount++) {
+        const row = []
+        rotatedShape.push(row)
+    }
+    for (let i = shape.length - 1, k = 0; i >= 0; i--, k++) {
+        for (let j = 0; j < shape[0].length; j++) {
+            rotatedShape[j][k] = shape[i][j]
+        }
+    }
 
+    return rotatedShape
+}
 
+function removePreviousShape() {
+    const rowsToColor = currentShape.shape.length;
+    const cellsToColor = currentShape.shape[0].length;
 
+    for (let rowIndex = 0; rowIndex < rowsToColor; rowIndex++) {
+        const row = tetrisPlaygroundTarget.children[rowIndex];
+        if (!row) continue; 
 
+        for (let cellIndex = 0; cellIndex < cellsToColor; cellIndex++) {
+            const cell = row.children[cellIndex];
+            if (cell && currentShape.shape[rowIndex][cellIndex]) {
+                cell.style.backgroundColor = ''; 
+            }
+        }
+    }
+}
 
+renderShape()
 
-
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        removePreviousShape()
+        currentShape.shape = rotateShape(currentShape.shape)
+        renderShape()
+    }
+})
---
+++ shapes.js
@@ -0,0 +1,46 @@
+export const shapes = {
+    O:{ 
+        shape:[[1, 1],
+               [1, 1]],
+        color: 'yellow'
+    },
+
+    J:{ 
+        shape:[[0, 1],
+               [0, 1],
+               [1, 1]],
+        color: 'navy'
+    },
+
+    I: { 
+        shape:[[1],
+               [1],
+               [1],
+               [1]],
+        color: 'lightblue'
+    },
+
+    L: { 
+        shape:[[1, 0],
+               [1, 0],
+               [1, 1]],
+        color: 'orange'
+    },
+    S: { 
+        shape:[[0, 1, 1],
+               [1, 1, 0]],
+        color: 'yellowgreen'
+    },
+
+    T: { 
+        shape:[[1, 1, 1],
+               [0, 1, 0]],
+        color: 'purple'
+    },
+
+    Z:{ 
+        shape:[[1, 1, 0],
+               [0, 1, 1]],
+        color: 'red'
+    }
+}
